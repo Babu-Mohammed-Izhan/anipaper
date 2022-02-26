2bc3ee0b8942573d347d09ff5d17d160ffa3379a
Izhan <babuizhan1234@gmail.com>
2022-02-26 23:22:20 +0530

    Fixed bugs

--- App.js
+++ App.js
@@ -2,50 +2,51 @@ import { useState, useEffect } from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
-const [animes, setanimes] = useState([]);
+const HomeScreen = () => {
+  const [animes, setanimes] = useState([]);
 
-const [animedata, setAnimedata] = useState({
-  type: 'anime',
-  page: 1,
-  subtype: 'tv',
-});
+  const [animedata, setAnimedata] = useState({
+    type: 'anime',
+    page: 1,
+    subtype: 'tv',
+  });
 
-const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
 
-const [active, setActive] = useState(1);
+  const [active, setActive] = useState(1);
 
-const getData = async ({ type, page, subtype }) => {
-  const data = await axios.get(
-    `https://api.jikan.moe/v3/top/${type}/${page}/${subtype}`
-  );
-  return data.data;
-};
+  const getData = async ({ type, page, subtype }) => {
+    const data = await axios.get(
+      `https://api.jikan.moe/v3/top/${type}/${page}/${subtype}`
+    );
+    return data.data;
+  };
 
-const searchData = async ({ type, search }) => {
-  const data = await axios.get(
-    `https://api.jikan.moe/v3/search/${type}?q=${search}&page=${active}`
-  );
-  return data.data;
-};
-async function fetchanime() {
-  const data = await getData(animedata);
-  console.log(data);
-  setanimes(data.top);
-}
-
-async function searchanime() {
-  if (search !== '') {
-    const data = await searchData({ type: animedata.type, search });
+  const searchData = async ({ type, search }) => {
+    const data = await axios.get(
+      `https://api.jikan.moe/v3/search/${type}?q=${search}&page=${active}`
+    );
+    return data.data;
+  };
+  async function fetchanime() {
+    const data = await getData(animedata);
     console.log(data);
-    setanimes(data.results);
+    setanimes(data.top);
   }
-}
 
-const HomeScreen = () => (
-  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category="h1">HOME</Text>
-  </Layout>
-);
+  async function searchanime() {
+    if (search !== '') {
+      const data = await searchData({ type: animedata.type, search });
+      console.log(data);
+      setanimes(data.results);
+    }
+  }
+  return (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text category="h1">Hello</Text>
+    </Layout>
+  );
+};
 
 export default () => (
   <ApplicationProvider {...eva} theme={eva.light}>
